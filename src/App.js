e0000c95996d0271f223859cf0b7be10b3e53714
Seichi <cngthnh22112002@gmail.com>
2023-03-21 08:58:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
 
 function App() {
+  const [light_sensor, setLightSensor] = useState('');
+  const [soilmoisture_sensor, setSoilmoistureSensor] = useState('');
+  const [humidity_sensor, setHumiditySensor] = useState('');
+  const [temperature_sensor, setTemperatureSensor] = useState('');
+
+  useEffect(() => {
+    socket.on('light-sensor', (data) => {
+      setLightSensor(data);
+    });
+    socket.on('soilmoisture-sensor', (data) => {
+      setSoilmoistureSensor(data);
+    });
+    socket.on('humidity-sensor', (data) => {
+      setHumiditySensor(data);
+    });
+    socket.on('temperature-sensor', (data) => {
+      setTemperatureSensor(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Message from light_sensor: {light_sensor}</p>
+      <p>Message from soilmoisture_sensor: {soilmoisture_sensor}</p>
+      <p>Message from humidity_sensor: {humidity_sensor}</p>
+      <p>Message from temperature_sensor: {temperature_sensor}</p>
     </div>
   );
 }
