f70fa03afaa516f71a2bc668ac121af47f142a27
long0339637721 <127864774+long0339637721@users.noreply.github.com>
2023-03-26 19:08:12 +0700

    Add files via upload

---
+++ src/pages/Pump.js
@@ -0,0 +1,11 @@
+// import React, { useEffect, useState } from 'react';
+// import io from 'socket.io-client';
+// import './Pump.css';
+
+export default function Pump() {
+  return(
+    <>
+      <h1>Pump</h1>
+    </>
+  )
+}
